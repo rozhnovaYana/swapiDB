afd9d98dde5017daa3496b7bda7ff90f3a31e01b
Yana <scream.yv@gmail.com>
2021-08-14 21:25:16 +0300

    random-planet component was connect to API

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,23 +1,56 @@
-import React from "react"
+import React, { Component } from "react"
 import classes from "./random-planet.module.css"
-const RandomPlanet=()=>{
-    return(
-        <div className={classes.randomPlanet}>
-            <div>
-                <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"/>
+import SwapiService from "../../services/swapi-service"
+class RandomPlanet extends Component{
+    state={
+        name:null,
+        population:null,
+        rotationPeriod:null,
+        diameter:null,
+        id:null
+    }
+    constructor(){
+        super()
+        this.updatePlanet()
+    }
+    service=new SwapiService()
+    updatePlanet=()=>{
+        let id=Math.floor(Math.random()*25)+5
+        this.service.getPlanet(id)
+        .then(planet=>{
+            this.setState({
+                id,
+                name:planet.name,
+                population:planet.population,
+                rotationPeriod:planet.rotation_period,
+                diameter:planet.diameter 
+            })
+        })
+    }
+    render(){
+        const{id, name, population, rotationPeriod, diameter}=this.state;
+        return(
+            <div className={classes.randomPlanet}>
+                <div>
+                    <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}/>
+                </div>
+                <div className={classes.randomPlanetText}>
+                    <div>
+                        {name}
+                    </div>
+                    <div>
+                        <b>Population: </b>{population}
+                    </div>
+                    <div>
+                        <b>Rotation period: </b>{rotationPeriod}
+                    </div>
+                    <div>
+                        <b>Diameter: </b>{diameter}
+                    </div>
+                </div>
             </div>
-            <div className={classes.randomPlanetText}>
-                <div>
-                    Title
-                </div>
-                <div>
-                    Title
-                </div>
-                <div>
-                    Title
-                </div>
-            </div>
-        </div>
-    )
+        )
+    }
+    
 }
 export default RandomPlanet;
